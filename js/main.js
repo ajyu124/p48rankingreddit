736a4d1c6789e86c85d602fdce083d5e7a0703be
Angus Jyu <angusjyu@yahoo.com>
2018-06-23 00:19:40 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var padding = 40;
 var middlePadding = (padding * 2) + 100;
 var width = $(window).width() - middlePadding - CHART_WIDTH - 30;
 
-var episodes = [1, 1.5, 2, 3, 5, 6, 8, 10, 11];
+var episodes = [1, 1.5, 2];
 var totalData;
 var dFirst;
 
